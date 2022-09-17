9dc7647809a2b6b13c994bf0c4d7214ce9d31696
Tamal Saha <tamal@appscode.com>
2022-09-17 00:41:30 -0700

    K8s 1.25.1
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- refresh-repo.sh
+++ refresh-repo.sh
@@ -5,12 +5,12 @@ SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
-PR_BRANCH=dbc22 # -$(date +%s)
-COMMIT_MSG="Update db-client-go"
+PR_BRANCH=k1251 # -$(date +%s)
+COMMIT_MSG="Use k8s 1.25.1 libs"
 
 REPO_ROOT=/tmp/kubedb-repo-refresher
 
-KUBEDB_API_REF=${KUBEDB_API_REF:-1241b40d48ec513379b1b2aabd80215e9b93b319}
+KUBEDB_API_REF=${KUBEDB_API_REF:-b7b935978ac397bb8bd5feddc9c33ce5f9045a30}
 
 repo_uptodate() {
     # gomodfiles=(go.mod go.sum vendor/modules.txt)
@@ -27,7 +27,7 @@ refresh() {
     rm -rf $REPO_ROOT
     mkdir -p $REPO_ROOT
     pushd $REPO_ROOT
-    git clone --no-tags --no-recurse-submodules --depth=1 https://${GITHUB_USER}:${GITHUB_TOKEN}@$1.git
+    git clone --no-tags --no-recurse-submodules --depth=1 git@github.com:$1.git
     name=$(ls -b1)
     cd $name
     git checkout -b $PR_BRANCH
@@ -40,7 +40,7 @@ go 1.18
 EOF
         # https://stackoverflow.com/a/63918676/244009
         # sed -i '' -e 's|github.com/jetstack/cert-manager|github.com/cert-manager/cert-manager|g' `grep 'jetstack' -rl *`
-        sed -i 's|github.com/jetstack/cert-manager|github.com/cert-manager/cert-manager|g' `grep 'jetstack' -rl *`
+        # sed -i 's|github.com/jetstack/cert-manager|github.com/cert-manager/cert-manager|g' `grep 'jetstack' -rl *`
 
         # sed -i "s|go 1.12|go 1.17|g" go.mod
         # sed -i "s|go 1.13|go 1.17|g" go.mod
@@ -54,27 +54,30 @@ EOF
         # fi
         go mod edit \
             -require=kubedb.dev/apimachinery@${KUBEDB_API_REF} \
-            -require=kubedb.dev/db-client-go@v0.0.2 \
-            -require=k8s.io/kube-openapi@v0.0.0-20220328201542-3ee0da9b0b42 \
-            -require=kmodules.xyz/resource-metadata@v0.12.5 \
+            -require=kubedb.dev/db-client-go@v0.0.4 \
+            -require=k8s.io/kube-openapi@v0.0.0-20220803162953-67bda5d908f1 \
+            -require=kmodules.xyz/resource-metadata@v0.13.0 \
             -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
             -require=gomodules.xyz/password-generator@v0.2.8 \
-            -require=go.bytebuilders.dev/license-verifier@v0.11.0 \
-            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.11.0 \
-            -require=go.bytebuilders.dev/audit@v0.0.23 \
-            -require=stash.appscode.dev/apimachinery@v0.22.0 \
-            -require=go.mongodb.org/mongo-driver@v1.9.1 \
-            -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@v0.12.2-0.20220603144237-6cd001896bf3 \
-            -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.5 \
-            -replace=k8s.io/apimachinery=github.com/kmodules/apimachinery@v0.24.2-rc.0.0.20220603191800-1c7484099dee \
-            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@v0.0.0-20220603223637-59dad1716c43 \
-            -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@v1.25.0-alpha.0.0.20220603172133-1c9d09d1b3b8
+            -require=go.bytebuilders.dev/license-verifier@v0.12.0 \
+            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.12.0 \
+            -require=go.bytebuilders.dev/audit@v0.0.24 \
+            -require=stash.appscode.dev/apimachinery@v0.22.1-0.20220917042332-522f4e18711d \
+            -require=github.com/elastic/go-elasticsearch/v7@v7.13.1 \
+            -require=go.mongodb.org/mongo-driver@v1.10.2 \
+            -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@a0df7cc5e5eef343a4664173516318666a48c02f \
+            -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
+            -replace=k8s.io/apiserver=github.com/kmodules/apiserver@4ac5fceca5180a267aa514a4041c03c7e6817f82 \
+            -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@54ac9f3f131541935cde59821f5a74036c0fe39d
 
         cat <<EOF >> go.mod
 replace (
+    go.opencensus.io => go.opencensus.io v0.23.0
     go.opentelemetry.io/contrib => go.opentelemetry.io/contrib v0.20.0
+    go.opentelemetry.io/contrib/instrumentation/github.com/emicklei/go-restful/otelrestful => go.opentelemetry.io/contrib/instrumentation/github.com/emicklei/go-restful/otelrestful v0.20.0
     go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc => go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.20.0
     go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp => go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.20.0
+    go.opentelemetry.io/contrib/propagators => go.opentelemetry.io/contrib/propagators v0.20.0
     go.opentelemetry.io/otel => go.opentelemetry.io/otel v0.20.0
     go.opentelemetry.io/otel/exporters/otlp => go.opentelemetry.io/otel/exporters/otlp v0.20.0
     go.opentelemetry.io/otel/metric => go.opentelemetry.io/otel/metric v0.20.0
@@ -105,7 +108,7 @@ EOF
         else
             git commit -a -s -m "$COMMIT_MSG"
         fi
-        git push -u origin $PR_BRANCH -f
+        git push -u origin HEAD -f
         hub pull-request \
             --labels automerge \
             --message "$COMMIT_MSG" \
