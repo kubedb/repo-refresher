e66f8d0f3caec15be386bbbc32247b56df983ae5
Tamal Saha <tamal@appscode.com>
2024-01-06 03:12:52 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- dep-refresh.sh
+++ dep-refresh.sh
@@ -5,12 +5,12 @@ SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 GITHUB_USER=${GITHUB_USER:-1gtm}
-PR_BRANCH=k129-auto # -$(date +%s)
+PR_BRANCH=k129-2 # -$(date +%s)
 COMMIT_MSG="Update deps"
 
 REPO_ROOT=/tmp/kubedb-repo-refresher
 
-API_REF=${API_REF:-a1d475ceb73e12977cce84eb7564393b9ae9b6e3}
+API_REF=${API_REF:-87c402a1a}
 
 repo_uptodate() {
     # gomodfiles=(go.mod go.sum vendor/modules.txt)
@@ -57,14 +57,14 @@ EOF
             -require=kubedb.dev/db-client-go@v0.0.8 \
             -require=kubestash.dev/apimachinery@cc46ddfd674a760d87ec2fe4122f7816296654c8 \
             -require=gomodules.xyz/logs@v0.0.7 \
-            -require=kmodules.xyz/client-go@v0.29.4 \
-            -require=kmodules.xyz/resource-metadata@v0.18.1 \
+            -require=kmodules.xyz/client-go@92f92b4d698588a735ae5b1a82329cfa81a20fc9 \
+            -require=kmodules.xyz/resource-metadata@e92a8a48d4005ebf72dc386868c068131e00f8b5 \
             -require=kmodules.xyz/go-containerregistry@v0.0.12 \
             -require=gomodules.xyz/password-generator@v0.2.9 \
             -require=go.bytebuilders.dev/license-verifier@v0.13.4 \
             -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.13.4 \
             -require=go.bytebuilders.dev/license-proxyserver@31122ab825027d2495c9320b63d99660f1ca56be \
-            -require=go.bytebuilders.dev/audit@3ff33160c6f02f6151e59cdd44dd50a347c02ba0 \
+            -require=go.bytebuilders.dev/audit@9cf3195 \
             -require=github.com/cert-manager/cert-manager@v1.13.3 \
             -require=github.com/elastic/go-elasticsearch/v7@v7.15.1 \
             -require=go.mongodb.org/mongo-driver@v1.10.2 \
