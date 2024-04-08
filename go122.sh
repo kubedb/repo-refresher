0781b52232058b85d797bb14060d14a8904e45cf
Tamal Saha <tamal@appscode.com>
2024-04-08 12:28:14 -0700

    wip
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- go122.sh
+++ go122.sh
@@ -53,6 +53,32 @@ refresh() {
         popd
     }
 
+    if [ -f go.mod ]; then
+        go mod edit \
+            -require=kmodules.xyz/client-go@v0.29.13 \
+            -require=kmodules.xyz/resource-metadata@v0.18.2 \
+            -require=gomodules.xyz/password-generator@v0.2.9 \
+            -require=go.bytebuilders.dev/license-verifier@v0.14.0 \
+            -require=go.bytebuilders.dev/license-verifier/kubernetes@v0.14.0 \
+            -require=go.bytebuilders.dev/license-proxyserver@v0.0.9 \
+            -require=go.bytebuilders.dev/audit@v0.0.33 \
+            -require=golang.org/x/net@v0.23.0 \
+            -require=github.com/golang/protobuf@v1.5.4 \
+            -require=google.golang.org/protobuf@v1.33.0
+            # -replace=github.com/Masterminds/sprig/v3=github.com/gomodules/sprig/v3@v3.2.3-0.20220405051441-0a8a99bac1b8 \
+            # -replace=sigs.k8s.io/controller-runtime=github.com/kmodules/controller-runtime@ac-0.17.0 \
+            # -replace=github.com/imdario/mergo=github.com/imdario/mergo@v0.3.6 \
+            # -replace=k8s.io/apiserver=github.com/kmodules/apiserver@ac-1.29.0 \
+            # -replace=k8s.io/kubernetes=github.com/kmodules/kubernetes@ac-1.29.0 \
+            # -require=github.com/docker/docker@v24.0.9+incompatible \
+            # -require=github.com/docker/cli@v24.0.9+incompatible
+
+        # sed -i 's|NewLicenseEnforcer|MustLicenseEnforcer|g' `grep 'NewLicenseEnforcer' -rl *`
+        go mod tidy
+        go mod vendor
+    fi
+    make fmt || true
+
     if repo_uptodate; then
         echo "Repository $1 is up-to-date."
     else
